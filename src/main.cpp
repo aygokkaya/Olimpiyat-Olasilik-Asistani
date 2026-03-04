bc61e4f4ff0bc3bc9b9c2b02daa6d6aa9365d77a
AHMET YASİN GÖKKAYA <151667722+aygokkaya@users.noreply.github.com>
2026-03-05 01:07:52 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <ctime>
+#include <cmath>
+#include <clocale>
 
+using namespace std;
+
+void BaslangicEkrani () {
+  setlocale(LC_ALL, "Turkish");
+  cout << "===========================================" << endl;
+  cout << "        OLİMPİYAT OLASILIK ASİSTANI        " << endl;
+  cout << "                 AYG LABS                  " << endl;
+  cout << "===========================================" << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+}
+int main () {
+  
